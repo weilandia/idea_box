5b0cbd624c7c80a98b5eea948007593c1443c4ba
Nick Weiland <nickweiland@gmail.com>
2016-05-12 12:20:17 -0600

    add filter tests

---
+++ app/assets/javascripts/ideas/idea_filter.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  var ideaFilter = function(filter) {
+    $(".idea").each(function() {
+      $(this).show()
+      if (filter === "all") return;
+      var quality = $(this).attr('data-quality')
+      if (quality != filter) {
+        $(this).toggle()
+      }
+    })
+  }
+
+  $(".active-filter").toggle()
+
+  $(document).on('click','.filter-button',function() {
+    $(".active-filter").toggle().removeClass("active-filter");
+
+    $(this).addClass("active-filter").toggle();
+
+    ideaFilter($(this).val());
+  });
+});

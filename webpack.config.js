6db7c0f42011a60fda0483dc8cdb0db63fa0a7c2
Nick Weiland <nickweiland@gmail.com>
2016-05-30 18:20:31 -0500

    setup react with sinatra

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     ],
   },
   output: {
-    path: path.join(__dirname, './app/assets'),
+    path: path.join(__dirname, './app/assets/javascripts'),
     filename: 'react_app.js',
   },
   plugins: [
